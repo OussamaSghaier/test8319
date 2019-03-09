a15c89ab3ba00583c6e79519203db8aff94d448b
oussama <oussamasghaier1995@gmail.com>
2019-03-08 23:24:19 -0500

    commit 4 branch1

--- microservices-tp4/discovery-service/src/main/java/tn/insat/tpmicro/discoveryservice/DiscoveryServiceApplication.java
+++ microservices-tp4/discovery-service/src/main/java/tn/insat/tpmicro/discoveryservice/DiscoveryServiceApplication.java
@@ -10,5 +10,8 @@ public class DiscoveryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiscoveryServiceApplication.class, args);
+		int b;
+		b--;
+		b=0;
 	}
 }
